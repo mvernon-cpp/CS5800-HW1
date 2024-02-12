9df6487f237ca5ba536081433be287fbf8e2fca4
Moina Vernon <mvernon@cpp.edu>
2024-02-11 19:10:18 -0800

    Completed Q3 (two instructors and two textbooks)

--- src/Q3/Course.java
+++ src/Q3/Course.java
@@ -5,14 +5,19 @@ public class Course {
 	private Instructor instructor;
 	private Textbook textbook;
 
+	private Instructor[] instructors;
+	private Textbook[] textbooks;
+
 	public Course(String courseName, Instructor instructor, Textbook textbook) {
 		this.courseName = courseName;
 		this.instructor = instructor;
 		this.textbook = textbook;
 	}
 
-	public Course(String courseName) {
+	public Course(String courseName, Instructor[] instructors, Textbook[] textbooks) {
 		this.courseName = courseName;
+		this.instructors = instructors;
+		this.textbooks = textbooks;
 	}
 
 	public String getCourseName() {
@@ -25,7 +30,22 @@ public class Course {
 
 	@Override
 	public String toString() {
-		return "Course Name:" + getCourseName() + "\n\tInstructor(s): " + instructor.toString() + "\n\tTextbook(s): "
+		return "Course Name: " + getCourseName() + "\n\tInstructor:\t" + instructor.toString() + "\n\tTextbook:\t"
 				+ textbook.toString();
+	}
+
+	public String print() {
+		String output = "Course Name: " + getCourseName() + "\n\tInstructor(s):\t";
+
+		for (Instructor instructor : instructors) {
+			output += instructor.toString() + "\t";
+		}
+
+		output += "\n\tTextbook(s):\t";
+		for (Textbook textbook : textbooks) {
+			output += textbook.toString() + "\t";
+		}
+
+		return output;
 	}
 }
--- src/Q3/Main.java
+++ src/Q3/Main.java
@@ -2,12 +2,18 @@ package Q3;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Instructor NDavarpanah = new Instructor("Nima", "Davarpanah", "8-15");
+		Instructor NDavarpanah = new Instructor("Nima", "Davarpanah", "3-2636");
 		Textbook CleanCode = new Textbook("Clean Code", "Robert C. Martin", "Pearson");
-		Course CS5800 = new Course("CS5800", NDavarpanah, CleanCode);
+		Course CS5800_01 = new Course("CS5800_01", NDavarpanah, CleanCode);
+
+		Instructor[] instructors = { NDavarpanah,
+				new Instructor("David", "Johansen", "8-13") };
+		Textbook[] textbooks = { CleanCode,
+				new Textbook("Design Patterns", "Erich Gamma", "Pearson") };
+		Course CS5800_02 = new Course("CS5800_02", instructors, textbooks);
 
 		System.out.println("Course(s) Information:");
-
-		System.out.println(CS5800);
+		System.out.println(CS5800_01 + "\n");
+		System.out.println(CS5800_02.print());
 	}
 }
